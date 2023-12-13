a0e9d6a9a81e8646a25b8f21b5eecdfde8493cf6
Czaruko <cezary.mildner98@gmail.com>
2023-12-13 02:54:33 +0100

    Finished manipulation of favorite pokemon

--- src/app/pages/favorites/favorites.component.ts
+++ src/app/pages/favorites/favorites.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { PokemonService } from '../../services/pokemon.service';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-favorites',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatListModule, MatIconModule, MatButtonModule],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.css',
 })
 export class FavoritesComponent {
-  constructor(public pokemonService: PokemonService) {}
+  constructor(public pokemonService: PokemonService, public router: Router) {}
 }
